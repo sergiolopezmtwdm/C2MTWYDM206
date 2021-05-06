ee078dcb0d8d3ccb2a9cfd38cf1f42d88b9fc23a
Sergio Lopez <sergiolopez.mtwdm@gmail.com>
2021-05-06 12:17:34 -0500

    creado el proyecto node

---
+++ API-C2MTWYDM206-NODE/environments/env.ts
@@ -0,0 +1,15 @@
+export default {
+    API: {
+        PORT: 3000
+    },
+    MONGODB: {
+        HOST: 'localhost',
+        PORT: 27017,
+        USER_NAME: '',
+        USER_PASSWORD: '',
+        DATABASE: 'dbMTWDyM'
+    },
+    TOKEN: {
+        EXPIRES: 60 * 60 * 4 // 4 HORAS
+    }
+};
--- API-C2MTWYDM206-NODE/index.ts
+++ API-C2MTWYDM206-NODE/index.ts
@@ -1,1 +1,24 @@
-console.log("estoy en node");
+import http from 'http';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import compression from 'compression'
+// import { Socket } from 'socket.io';
+import ENV from './environments/env';
+
+
+const app = express();
+app.use(express.json());
+app.use(compression());
+app.use(cors({ origin: true, credentials: true }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'API Real-Time funcionando correctamente'
+    });
+})
+
+
+app.listen(ENV.API.PORT, () => {
+    console.log(`Servidor Express funcionando correctamente en puerto ${ENV.API.PORT}`);
+});
