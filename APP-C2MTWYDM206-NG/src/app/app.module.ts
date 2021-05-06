591ec192a42970ae09580f5bcc52a6a206cfa670
Sergio Lopez <sergiolopez.mtwdm@gmail.com>
2021-05-06 17:21:04 -0500

    agregado http al nombre del contenedor de node para ver si conecta

--- APP-C2MTWYDM206-NG/src/app/app.module.ts
+++ APP-C2MTWYDM206-NG/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
 
 // const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
-const config: SocketIoConfig = { url: 'API-C2MTWYDM206-NODE', options: {} };
+const config: SocketIoConfig = { url: 'http://API-C2MTWYDM206-NODE', options: {} };
 
 @NgModule({
   declarations: [
