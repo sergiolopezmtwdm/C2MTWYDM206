77f412055dfc1cc67d30e9385177e8e55852166a
Sergio Lopez <sergiolopez.mtwdm@gmail.com>
2021-05-06 17:10:47 -0500

    cambiado la url para que angular conecte a node desde aws

--- APP-C2MTWYDM206-NG/src/app/app.module.ts
+++ APP-C2MTWYDM206-NG/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+// const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'API-C2MTWYDM206-NODE', options: {} };
 
 @NgModule({
   declarations: [
