4acd32de289d65135e60c1897ca02e4ad43728e1
Sergio Lopez <sergiolopez.mtwdm@gmail.com>
2021-05-06 17:25:57 -0500

    cambiada la URL para conectar al servicio de sockets

--- APP-C2MTWYDM206-NG/src/app/app.module.ts
+++ APP-C2MTWYDM206-NG/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
 
 // const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
-const config: SocketIoConfig = { url: 'http://API-C2MTWYDM206-NODE', options: {} };
+// const config: SocketIoConfig = { url: 'http://API-C2MTWYDM206-NODE', options: {} };
+const config: SocketIoConfig = { url: 'http://slopezfu-node.com', options: {} };
 
 @NgModule({
   declarations: [
