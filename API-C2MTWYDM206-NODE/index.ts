f4b7f042f804f434de995ffa580e6a4a629c0b28
Sergio Lopez <sergiolopez.mtwdm@gmail.com>
2021-05-06 12:43:31 -0500

    se agrego el helper para mongo db y se inicio la configuración de socket io, funcionalidad para real time

--- API-C2MTWYDM206-NODE/index.ts
+++ API-C2MTWYDM206-NODE/index.ts
@@ -2,23 +2,51 @@ import http from 'http';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import compression from 'compression'
-// import { Socket } from 'socket.io';
+import { Socket } from 'socket.io';
 import ENV from './environments/env';
+import MongoHelper from "./helpers/mongo.helper";
+
+const mongo = MongoHelper.getInstance(ENV.MONGODB);
+// const tokenHelper = TokenHelper(ENV, mongo);
+
+(async () => {
+    const app = express();
+    app.use(express.json());
+    app.use(compression());
+    app.use(cors({ origin: true, credentials: true }));
+
+    app.get('/', (req: Request, res: Response) => {
+        res.status(200).json({
+            ok: true,
+            msg: 'API Real-Time funcionando correctamente'
+        });
+    })
 
 
-const app = express();
-app.use(express.json());
-app.use(compression());
-app.use(cors({ origin: true, credentials: true }));
+    // MANEJO DE SOCKTES
+    const httpServer = http.createServer(app);
+    const socketIO = require('socket.io')(httpServer);
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({
-        ok: true,
-        msg: 'API Real-Time funcionando correctamente'
+    // Funcionalidad Real-Time        
+    socketIO.on('connection', (socket: Socket) => {
+        // TO DO: Lógica Real-Time
+        console.log(`Nuevo cliente conectado con ID: ${socket.id}`);
+        console.log(`Aquí se detecta una nueva conexión de un cliente y se guarda en base de datos con ID: ${socket.id}`);
     });
-})
 
+    // app.listen(ENV.API.PORT, () => {
+    httpServer.listen(ENV.API.PORT, () => {
+        console.log(`Servidor Express funcionando correctamente en puerto ${ENV.API.PORT}`);
+    });
+    
+})();
 
-app.listen(ENV.API.PORT, () => {
-    console.log(`Servidor Express funcionando correctamente en puerto ${ENV.API.PORT}`);
+// Handle Error
+process.on('unhandleRejection', (error: any, promise) => {
+    console.log(`Ocurrió un error no controlado de tipo promise rejection`, promise);
+    console.log(`La descripción de error es la siguiente`, error);
+    // Close Mongo
+    mongo.close();
+    process.exit();
 });
+
